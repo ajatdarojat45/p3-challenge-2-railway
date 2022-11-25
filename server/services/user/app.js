cf40dfb3ad24cff122720737feac015df43fdbc9
Ajat Darojat <ajatdarojat45@gmail.com>
2022-11-25 14:36:47 +0700

    connect to db

--- server/services/user/app.js
+++ server/services/user/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const pgp = require("pg-promise")(/* options */);
+const db = pgp(
+  "postgres://postgres:QNJ5j76djhN8WhDm@db.kegkriwnpgzuvjxhcfmu.supabase.co:5432/postgres"
+);
 
+// QNJ5j76djhN8WhDm
 app.get("/", (_, res) => {
   res.status(200).json({ msg: "user service" });
+});
+
+app.get("/users", (_, res) => {
+  db.query("SELECT * FROM users")
+    .then((data) => {
+      res.status(200).json({ users: data });
+    })
+    .catch((err) => {
+      res.status(500).json({ err });
+    });
 });
 
 app.listen(PORT, () => {
